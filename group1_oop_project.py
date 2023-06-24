d8b6ab66e68e1e2913ac76cc7ce2bdbfcdc593f0
avelbryl <96860802+avelbryl@users.noreply.github.com>
2023-06-25 00:45:18 +0800

    Add files via upload

--- group1_oop_project.py
+++ group1_oop_project.py
@@ -1,14 +1,11 @@
-class Pelanggan:
-    def __init__(self,nama,tanggal,jam_mulai,jam_berakhir,slot):
-        self.nama = nama
-        self.tanggal = tanggal
-        self.jam_mulai = jam_mulai
-        self.jam_berakhir = jam_berakhir
-        self.slot = slot
-    
-    def __str__(self):
-        return f"\nNama: {self.nama}\nTanggal: {self.tanggal}\nJam mulai: {self.jam_mulai}\nJam berakhir: {self.jam_berakhir}\nSlot: {self.slot}"
+import csv
 
+class Pelanggan:
+    def __init__(self,nama,jam,durasi):
+        self.nama = nama
+        self.jam = jam
+        self.durasi = durasi
+    
     @property
     def nama(self):
         return self._nama
@@ -18,76 +15,160 @@ class Pelanggan:
         self._nama = nama
 
     @property
-    def tanggal(self):
-        return self._tanggal
+    def jam(self):
+        return self._jam
     
-    @tanggal.setter
-    def tanggal(self, tanggal):
-        self._tanggal = tanggal
-
+    @jam.setter
+    def jam(self, jam):
+        jam = int(jam.removesuffix('.00'))
+        self._jam = jam
+    
     @property
-    def jam_mulai(self):
-        return self._jam_mulai
+    def durasi(self):
+        return self._durasi
     
-    @jam_mulai.setter
-    def jam_mulai(self, jam_mulai):
-        self._jam_mulai = jam_mulai
-
-    @property
-    def jam_berakhir(self):
-        return self._jam_berakhir
-    
-    @jam_berakhir.setter
-    def jam_berakhir(self, jam_berakhir):
-        self._jam_berakhir = jam_berakhir
-
-    @property
-    def slot(self):
-        return self._slot
-    
-    @slot.setter
-    def slot(self, slot):
-        self._slot = slot
-
-    def pesan(self):
-        ...
+    @durasi.setter
+    def durasi(self, durasi):
+        self._durasi = durasi
 
     def cancel(self):
         ...
 
     def bayar(self):
-        ...
+        print("- Biaya 1 jam adalah Rp. 150.000")
+        harga1jam = 150000
+        print("Harga yang harus dibayar = Rp",int(harga1jam*self.jam))
+
+class PelangganLama(Pelanggan):
+    def __init__ (self, nama):
+        self.nama = nama
+
+    
+    # Fungsi bayar() di override
+    def bayar(self):
+        print("- Biaya 1 jam adalah Rp. 100.000")
+        harga1jam = 100000
+        print("Harga yang harus dibayar = Rp",int(harga1jam*self.jam))
+
 
     
 class RentalLapangan:
-    def __init__(self):
-        ...
+    daftar = []
 
-    def tambah_pelanggan(self):
-        ...
+    def __init__(self,pelanggan=None):
+        self.pelanggan = pelanggan
     
-    def isi_data_pelanggan(self):
-        ...
+    def isi_data_pelanggan(self,pelanggan):
+        # buat empty list untuk mengisi jam2
+        jam_mulai = []
+        m = 0
+
+        # masukkan setiap 1 jam ke dalam list
+        for _ in range(getattr(self.pelanggan,'durasi')):
+            jam_mulai.append(getattr(self.pelanggan,'jam')+m)
+            m+=1
+
+        available = 'yes'
+        # cek apakah jam yang dipesan pelanggan tersedia
+        for slot in self.daftar:
+            if slot['jam'] in jam_mulai:
+                available = 'no'
+                break
+        
+        if available == 'yes':
+            # apabila jam yang diinput dalam jangka waktu jam 10 pagi hingga jam 10 malam maka masukkan data pelanggan
+            if jam_mulai[0] >= 10 and jam_mulai[-1] <= 21:
+                j = 0
+                for _ in range(getattr(self.pelanggan,'durasi')):
+                    new_data = {}
+                    new_data["nama"] = getattr(self.pelanggan,'nama')
+                    new_data["jam"] = getattr(self.pelanggan,'jam')+j
+                    new_data["durasi"] = getattr(self.pelanggan,'durasi')
+                    self.daftar.append(new_data)
+                    j+=1
+                return True
+            else:
+                print("Maaf kami tidak melayani reservasi sebelum jam 10 pagi atau setelah jam 10 malam")
+                return False
+        else:
+            print("Maaf slot ini sudah direservasi\n")
+            return False
+
     
+    def rincian_pemesanan(self,pelanggan):
+        print("\nRincian pemesanan")
+        print("======================")
+        print(f"Nama: {pelanggan.nama}\nJam mulai: {pelanggan.jam}.00\nJam berakhir: {pelanggan.jam+pelanggan.durasi}.00\n======================\n")
+
+
     def verifikasi_pembayaran(self):
         ...
     
     def tampilkan_daftar(self):
-        ...
+        print("Nama\t\tJam mulai   Jam berakhir")
+        print("=========================================")
+        for slot in self.daftar:
+            # print nama
+            print(slot['nama'].title(),end="")
+            if len(slot['nama']) < 16:
+                for _ in range(16 - len(slot['nama'])):
+                    print(" ",end="")
+                print(str(slot['jam'])+".00\t   ",str(slot['jam']+1)+".00")
 
-def input_pelanggan():
+
+    
+def input_data():
     nama = input("Nama: ")
-    tanggal = input("Tanggal: ")
-    jam_mulai = input("Jam mulai: ")
-    jam_berakhir = input("Jam berakhir: ")
-    slot = input("Slot: ")
-    return Pelanggan(nama,tanggal,jam_mulai,jam_berakhir,slot)
+    jam = input("Mulai jam berapa: ")
+    durasi = int(input("Berapa jam?: "))
+    return nama,jam,durasi
+
+
+def menu():
+    print('Selamat datang!')
+    print("1. Booking lapangan")
+    print("2. Jadwal reservasi")
+    print("3. Cancel reservasi")
+    print("4. Keluar")
+
+
+
 
 def main():
-    p1 = input_pelanggan()
-    print(p1)
+    menu()
+    pilihan = int(input(""))
+    while True:
+        if pilihan == 1:
+            # 1. input data calon pelanggan
+            nama,jam,durasi = input_data()
+            # 2. buat objek pelanggan dan rental
+            calon_p = Pelanggan(nama,jam,durasi)
+            rental = RentalLapangan(calon_p)
+            # 3. pelanggan melakukan pembayaran
+            ...
+            # 4. apabila pembayaran telah diverifikasi, rental mengisi data pelanggan
+            tersedia = rental.isi_data_pelanggan(calon_p)
+            if tersedia == True:
+                # 5. rental menampilkan rincian pemesanan pelanggan
+                rental.rincian_pemesanan(calon_p)
+            else:
+                pass
+        elif pilihan == 2:
+            rental = RentalLapangan()
+            print(rental.daftar)
+            rental.tampilkan_daftar()
+        elif pilihan == 3:
+            ...
+        elif pilihan == 4:
+            # simpan daftar ke file csv
+            break
+        else:
+            print("Mohon masukkan input yang sesuai!")
 
-   
+        
+        menu()
+        pilihan = int(input(""))
+
 
 if __name__ == "__main__":
     main()
